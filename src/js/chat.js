b750de6dffd86f9c29e30e92f8180c1c9d32c197
Steve007 <stevenag006@hotmail.com>
2015-12-19 18:20:36 -0500

    heroku sockio

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,7 +1,9 @@
 (function($) {
   var addMessage, clearOldMessages, currentMsg, filterMessage, filterWords, messages, socket, totalMsg, update;
   filterWords = ['esoteric', 'kissed', 'expansive', 'peccatophobia', 'liberalizing', 'dec', 'holophyte', 'tummeler', 'soloistic', 'incarnalized', 'lane', 'much', 'preobligating', 'hello', 'shirty', 'mastermind', 'wainage', 'immortality', 'undeniably', 'pushed', 'rudy', 'asdic', 'arlington', 'diverse', 'positivist', 'spellbind', 'albatross', 'overripen', 'lilibel', 'undepreciatory', 'enterozoan', 'smoko', 'kachina', 'lyse', 'predisguising', 'instead', 'iambus', 'oversight', 'kahului', 'pagliacci', 'hyperbrachycephalic', 'fedora', 'advertence', 'sunken', 'before', 'outmoding', 'chatham', 'encipher', 'recollectedness', 'klister', 'sassanids', 'prague', 'thyestes', 'meridional', 'umbria', 'didache', 'braunite', 'oppugnant', 'nondeceiving', 'yawner', 'nonsensitizing', 'disunite', 'ecad', 'berea', 'bushranging', 'perisarcous', 'bilharzia', 'automatist', 'daguerreotypist', 'overdedicating', 'hisself', 'pitchblende', 'radom', 'unsapiential', 'rubasse', 'cutting', 'epitomizer', 'kindredness', 'rocker', 'dacryorrhea', 'carpogonium', 'proximal', 'fitfulness', 'pigeonwing', 'irradiation', 'flank', 'dariole', 'lewes', 'mayag', 'delouse', 'lingcod', 'diazoalkane', 'unorganically', 'difficult', 'grav', 'bandolier', 'antep', 'curser', 'unnautical', 'jolly'];
-  socket = io.connect('http://localhost:3000');
+  socket = io.connect('https://localhost:3000', {
+    secure: true
+  });
   messages = $('.messages');
   totalMsg = [];
   currentMsg = 0;
